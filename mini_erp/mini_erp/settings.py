59e5ef7ed23997110ff813a38164f4162545fe07
Roshni Seth <roshniseth92@gmail.com>
2025-09-17 22:34:17 +0530

    Final check before deploy

--- mini_erp/mini_erp/settings.py
+++ mini_erp/mini_erp/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,13 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nl7e1%5q6(k4!ktlt_*0ut68coizsb*(wv&hii985hgih)5v#='
+# SECRET_KEY = 'django-insecure-nl7e1%5q6(k4!ktlt_*0ut68coizsb*(wv&hii985hgih)5v#='
+SECRET_KEY = config("SECRET_KEY", default="django-insecure-fallback-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
 # Application definition
 
@@ -77,21 +79,19 @@ WSGI_APPLICATION = 'mini_erp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# Default: local SQLite (for easy development)
 DATABASES = {
-    # 'default': {
-    #     # 'ENGINE': 'django.db.backends.sqlite3',
-    #     # 'NAME': BASE_DIR / 'db.sqlite3',
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'mini_erp',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'Roshni2001#',
-    #     'HOST': 'localhost',
-    #     'PORT': '5432',
-    # }
-    'default': dj_database_url.config(
-        default=os.environ.get("postgresql://mini_erp_db_gtgu_user:DTX3K0Uw6cVY5X49gCNHkAPsYTSa2Do7@dpg-d34q3jm3jp1c73egr7p0-a/mini_erp_db_gtgu")
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
+# If DATABASE_URL is provided (Render), override with Postgres
+DATABASE_URL = config("DATABASE_URL", default="")
+if DATABASE_URL:
+    DATABASES["default"] = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+
 
 
 # Password validation
@@ -128,8 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
